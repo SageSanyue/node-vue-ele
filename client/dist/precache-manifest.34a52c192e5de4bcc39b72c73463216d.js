1f5eb7da493ac1f63d408601d09429c18b23ea5a
SageSanyue <562441461@qq.com>
2019-08-05 00:32:57 +0800

    改接口s

---
+++ client/dist/precache-manifest.34a52c192e5de4bcc39b72c73463216d.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "7c9bef3cf30cf9fb8b78",
+    "url": "/assets/css/app.1d5b49da.css"
+  },
+  {
+    "revision": "7c9bef3cf30cf9fb8b78",
+    "url": "/assets/js/app.e55139ff.js"
+  },
+  {
+    "revision": "c76a5c42e0e98a6020af",
+    "url": "/assets/css/chunk-vendors.8675e282.css"
+  },
+  {
+    "revision": "c76a5c42e0e98a6020af",
+    "url": "/assets/js/chunk-vendors.b615d06d.js"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/assets/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/assets/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "62959fc5aa1723b306ad8a2466d2f6cf",
+    "url": "/assets/img/404.62959fc5.png"
+  },
+  {
+    "revision": "d76eea8ea01991feacba45cacd8a4913",
+    "url": "/assets/img/bg.d76eea8e.png"
+  },
+  {
+    "revision": "5b0dbe1becdc7753d3e1212228ea609c",
+    "url": "/assets/img/wall.5b0dbe1b.jpg"
+  },
+  {
+    "revision": "d0fa6a2baf8194aceba44b8c7c0dea93",
+    "url": "/assets/img/logo.d0fa6a2b.png"
+  },
+  {
+    "revision": "5829ee03df3cc7425e7f97b3db694e68",
+    "url": "/index.html"
+  },
+  {
+    "revision": "68ec8e3bbb1885054e9d8a4e950be7d3",
+    "url": "/css/reset.css"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
